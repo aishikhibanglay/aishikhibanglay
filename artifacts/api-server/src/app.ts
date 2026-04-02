8691c5d61f29df4bfc2ac82c3c26890fb3043714
aishikhibanglay <aishikhibanglay@gmail.com>
2026-04-02 08:12:43 -0700

    Vercel migration: artifacts/api-server/src/app.ts

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -59,7 +59,7 @@ app.use(
     cookie: {
       maxAge: 7 * 24 * 60 * 60 * 1000,
       httpOnly: true,
-      sameSite: isProd ? "none" : "lax",
+      sameSite: "lax",
       secure: isProd,
     },
   }),
